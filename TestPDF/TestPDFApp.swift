c65d63582bacc4483ec8bf0374d31747d85c1d10
John B <jbethancourt.ctr@revacomm.com>
2020-12-16 12:07:00 -1000

    Initial Commit

---
+++ TestPDF/TestPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPDFApp.swift
+//  TestPDF
+//
+//  Created by John Bethancourt on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct TestPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
